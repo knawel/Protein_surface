correct logP calculation

model/pytorch/03-train_pt.py
@@ -53,7 +53,7 @@ with open(args.pdb_list, 'r') as iFile:
     for i in iFile:
         if i[0] != '#':
             proteins.append(i.strip())
-train_prots, test_prots = train_test_split(proteins, train_size=train_f, random_state=12)
+train_prots, test_prots = train_test_split(proteins, train_size=train_f, random_state=seed)
 logger.print("\n")
 logger.print("Data")
 logger.print("-------------------------")
@@ -75,12 +75,12 @@ logger.print(f"Vertices: train {len(train_dataset)}   test {len(test_dataset)}")
 logger.print(f"Features: {n_features}")
 
 # Create data loaders.
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 model = AmberNN(n_features, n_cls, hid_size).to(device)
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)#, weight_decay=0.01)
 
 
 def train_loop(dataloader, model, loss_fn, optimizer):

model/pytorch/nn_model.py
@@ -3,28 +3,27 @@ import torch.nn.functional as F
 
 
 class AmberNN(nn.Module):
-    def __init__(self, input_size, num_classes, hidden_size, p=0.15):
+    def __init__(self, input_size, num_classes, hidden_size, p=0.10):
         super(AmberNN, self).__init__()
         # self.layer_norm = nn.LayerNorm(input_size)
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, hidden_size)
-        self.fc4 = nn.Linear(hidden_size, 64)
-        # self.fc5 = nn.Linear(64, 64)
+        self.fc4 = nn.Linear(hidden_size, 16)
+        self.fc5 = nn.Linear(16, 8)
         # self.fc6 = nn.Linear(64, 16)
-        self.fc7 = nn.Linear(64, num_classes)
+        self.fc7 = nn.Linear(8, num_classes)
         self.dropout = nn.Dropout(p)
 
     def forward(self, x):
         # output = self.layer_norm(x)
         out = F.relu(self.fc1(x))
-        # out = self.dropout(out)
         out = F.relu(self.fc2(out))
         out = self.dropout(out)
         out = F.relu(self.fc3(out))
-        out = self.dropout(out)
+        # out = self.dropout(out)
         out = F.relu(self.fc4(out))
-        # out = F.relu(self.fc5(out))
+        out = F.relu(self.fc5(out))
         # out = F.relu(self.fc6(out))
         out = F.relu(self.fc7(out))
         return out